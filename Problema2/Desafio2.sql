aad09896ee42a3a46ae529a93570249114154f6a
Luiz Felipe <166581273+luiz426@users.noreply.github.com>
2025-06-24 17:15:00 -0300

    Update Desafio2.sql

--- Problema2/Desafio2.sql
+++ Problema2/Desafio2.sql
@@ -2,7 +2,8 @@ INSERT INTO stg_prontuario.paciente
 SELECT * FROM stg_hospital_a.paciente;
 
 INSERT INTO stg_prontuario.paciente
-SELECT * FROM stg_hospital_b.paciente;
+SELECT * FROM stg_hospital_b.paciente
+WHERE CPF != NOT NULL;
 
 INSERT INTO stg_prontuario.paciente
 SELECT * FROM stg_hospital_c.paciente;
